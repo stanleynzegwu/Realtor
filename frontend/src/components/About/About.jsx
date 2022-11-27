c5400a104fa819d14b1c7f218d03e82a14cc2dc6
Stanley Nzegwu <nzegwustanley@gmail.com>
2022-11-27 21:14:19 +0100

    added media queries for mobile devices

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -8,14 +8,14 @@ const About = () => {
             <p className='about__text'>WHAT WE DO</p>
             <h2 className='about__header'>our main focus</h2>
             <div className='about__card-holder'>
-                {[{icon:<ImHome />,about:'Buy a Building',text:'lorem and we can jdhdh jhdhd jdjdj bcvdhdhd'},
-                {icon:<ImHome />,about:'Buy a Land',text:'lorem and we can jdhdh jhdhd jdjdj bcvdhdhd'},
-                {icon:<ImHome />,about:'Sell a land',text:'lorem and we can jdhdh jhdhd jdjdj bcvdhdhd'}].map(({icon,about,text},index) => (
+                {[{icon:<ImHome />,about:'Buy a Building',text:'lorem and we can hfhhfhf fhfhfhhdh hhhhdhdhdh dhdhdhdhh jdhdh jhdhd jdjdj bcvdhdhd'},
+                {icon:<ImHome />,about:'Buy a Land',text:'lorem and we nnvhfhfhf hdnc  c dhd c dshsh  dbdhdhdhhd dhd can jdhdh jhdhd jdjdj bcvdhdhd'},
+                {icon:<ImHome />,about:'Hire Painters',text:'lorem and we can jdhdh jhdhd jdjdj bcvdhdhd'}].map(({icon,about,text},index) => (
                     <div className='card' key={index}>
                         <div className='card__icon'>{icon}</div>
                         <p className='card__about'>{about}</p>
-                        <p>{text}</p>
-                        <div className='btn'><button>More</button></div>
+                        <p className='card__text'>{text}</p>
+                        <div className='btn card__button'><button>More</button></div>
                     </div>
                 ))}
             </div>
