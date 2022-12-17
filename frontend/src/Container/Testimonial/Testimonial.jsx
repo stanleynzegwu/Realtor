85df902d81200cbec4024cc748d8b93f3adb2218
Stanley Nzegwu <nzegwustanley@gmail.com>
2022-12-17 13:32:37 +0100

    used the string split method to get the main part email in the testimonial, so i can also diaplay it as a handle

--- frontend/src/Container/Testimonial/Testimonial.jsx
+++ frontend/src/Container/Testimonial/Testimonial.jsx
@@ -13,21 +13,21 @@ import { Autoplay, Pagination, Navigation } from "swiper";
 
 const arr = [
 {img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSy-DW2gcUJHDogOJbd573WTCTg9z7xjo47cq88JYPW&s',name:'David Lee',
-star:5,testimony:'One of the best real estate companies you could find out there. They are so professional'},
+email:'julian@gmail.com',star:5,testimony:'One of the best real estate companies you could find out there. They are so professional'},
 {img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSy-DW2gcUJHDogOJbd573WTCTg9z7xjo47cq88JYPW&s',name:'Brendan Eich',
-star:5,testimony:'I had the best time of my life ,when i went to checkout a land- it was like a tour and a guide assigned,Lol.'},
+email:'julian@gmail.com',star:5,testimony:'I had the best time of my life ,when i went to checkout a land- it was like a tour and a guide assigned,Lol.'},
 {img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSy-DW2gcUJHDogOJbd573WTCTg9z7xjo47cq88JYPW&s',name:'Thomas Tubee',
-star:4,testimony:'One of the best real estate companies you could find out there. They are so professional'},
+email:'julian@gmail.com',star:4,testimony:'One of the best real estate companies you could find out there. They are so professional'},
 {img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSy-DW2gcUJHDogOJbd573WTCTg9z7xjo47cq88JYPW&s',name:'Lionel Messi',
-star:5,testimony:'One of the best real estate companies you could find out there. They are so professional'},
+email:'julian@gmail.com',star:5,testimony:'One of the best real estate companies you could find out there. They are so professional'},
 {img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSy-DW2gcUJHDogOJbd573WTCTg9z7xjo47cq88JYPW&s',name:'Joe Biden',
-star:4,testimony:'One of the best real estate companies you could find out there. They are so professional'},
+email:'vallu@gmail.com',star:4,testimony:'One of the best real estate companies you could find out there. They are so professional'},
 {img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSy-DW2gcUJHDogOJbd573WTCTg9z7xjo47cq88JYPW&s',name:'Valentine Nwankwo',
-star:3,testimony:'One of the best real estate companies you could find out there. They are so professional'},
+email:'julian@gmail.com',star:3,testimony:'One of the best real estate companies you could find out there. They are so professional'},
 {img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSy-DW2gcUJHDogOJbd573WTCTg9z7xjo47cq88JYPW&s',name:'Ngozi Iweala',
-star:5,testimony:'One of the best real estate companies you could find out there. They are so professional'},
+email:'kula678@gmail.com',star:5,testimony:'One of the best real estate companies you could find out there. They are so professional'},
 {img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSy-DW2gcUJHDogOJbd573WTCTg9z7xjo47cq88JYPW&s',name:'Yvone Okoro Emerald',
-star:4,testimony:'One of the best real estate companies you could find out there. They are so professional'}
+email:'turak90@gmail.com',star:4,testimony:'One of the best real estate companies you could find out there. They are so professional'}
 ]
 
 const Testimonial = () => {
@@ -71,12 +71,13 @@ const Testimonial = () => {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-                {arr.map(({img,name,star,testimony},index) => (
+                {arr.map(({img,name,email,star,testimony},index) => (
                     <SwiperSlide className='testimonial-card' key={index}>
                         <div className='client__img'>
                             <img src={img} alt="client" />
                         </div>
                         <p className='client__name'>{name}</p>
+                        <p className='client__email'>{`${email.split('@')[0]}@`}</p>
                         <div className='client__rating'>
                             <span>{Array(star).fill(<AiFillStar />)}</span>
                         </div>
